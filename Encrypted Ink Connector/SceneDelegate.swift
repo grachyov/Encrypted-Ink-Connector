5b2515106d2680660fcc625957891fa5c77d2344
Ivan Grachyov <mail@grachyov.com>
2021-06-12 15:27:59 +0300

    Get links from qr code

--- Encrypted Ink Connector/SceneDelegate.swift
+++ Encrypted Ink Connector/SceneDelegate.swift
@@ -2,12 +2,25 @@
 
 import UIKit
 
+var launchURL: URL?
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        processURLContexts(connectionOptions.urlContexts)
         guard (scene as? UIWindowScene) != nil else { return }
     }
-
+    
+    private func processURLContexts(_ URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            launchURL = url
+        }
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        processURLContexts(URLContexts)
+    }
+    
 }
--- Encrypted Ink Connector/ViewController.swift
+++ Encrypted Ink Connector/ViewController.swift
@@ -13,9 +13,22 @@ class ViewController: UIViewController {
         startBlinking()
     }
 
+    func showAlertIfNeeded() {
+        guard launchURL != nil else { return }
+        let url = launchURL
+        launchURL = nil
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Hello", message: url?.absoluteString, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default) { _ in }
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
     func startBlinking() {
         Timer.scheduledTimer(withTimeInterval: 0.75, repeats: true) { [weak self] _ in
             self?.blinkingView.isHidden.toggle()
+            self?.showAlertIfNeeded()
         }
     }
     
