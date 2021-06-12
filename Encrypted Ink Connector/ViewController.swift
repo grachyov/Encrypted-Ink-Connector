62c90e413eb399fe1c45cf5bfa091491ad2ff702
Ivan Grachyov <mail@grachyov.com>
2021-06-12 13:18:25 +0300

    Initial Commit

---
+++ Encrypted Ink Connector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Encrypted Ink Connector
+//
+//  Created by Ivan Grachev on 12.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
