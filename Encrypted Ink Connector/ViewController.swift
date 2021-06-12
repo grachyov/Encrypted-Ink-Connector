a78b1a89a26580f9c79291221f10881d1ad6f042
Ivan Grachyov <mail@grachyov.com>
2021-06-12 14:16:25 +0300

    Implement UI

--- Encrypted Ink Connector/ViewController.swift
+++ Encrypted Ink Connector/ViewController.swift
@@ -4,9 +4,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var connectionLabel: UILabel!
+    @IBOutlet weak var blinkingView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        connectionLabel.text = "Connecting\nwith MacOS"
+        startBlinking()
     }
 
+    func startBlinking() {
+        Timer.scheduledTimer(withTimeInterval: 0.75, repeats: true) { [weak self] _ in
+            self?.blinkingView.isHidden.toggle()
+        }
+    }
+    
 }
