c0b8295e321f6216bd62be097a2b587ab5cfb922
Ivan Grachyov <mail@grachyov.com>
2021-06-13 11:11:20 +0300

    Better text for scan screen

--- Encrypted Ink Connector/ViewController.swift
+++ Encrypted Ink Connector/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        connectionLabel.text = "Connecting\nwith MacOS"
+        connectionLabel.text = "Scan QR code\nto connect"
         startBlinking()
         
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(showScanner))
